9ee2984e02bfbbb2279ef4178e16ab11785fab8b
ElliotLuque <elliotluque@gmail.com>
2022-08-25 01:02:44 +0200

    Initial code

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,10 @@ FROM python:3-slim AS builder
 ADD . /app
 WORKDIR /app
 
-# We are installing a dependency here directly into our app source dir
+# Dependencies
 RUN pip install --target=/app requests
+RUN pip install --target=/app beautifulsoup4
+
 
 # A distroless container image with Python and some basics like SSL certificates
 # https://github.com/GoogleContainerTools/distroless
--- main.py
+++ main.py
@@ -1,14 +1,22 @@
-import os
-import requests  # noqa We are just importing this to prove the dependency installed correctly
+import requests
+from bs4 import BeautifulSoup
 
 
 def main():
-    my_input = os.environ["INPUT_MYINPUT"]
 
-    my_output = f"Hello {my_input}"
+    url = "https://retosdeprogramacion.com/semanales2022"
+    req = requests.get(url)
 
-    print(f"::set-output name=myOutput::{my_output}")
+    soup = BeautifulSoup(req.content, 'html.parser')
 
+    challenge = soup.select('section.palette-1.section.section-left > div > div > article > h4 > span > span')[0].text
+
+    pos1 = challenge.find("#") + 1
+    pos2 = challenge.find(":", pos1)
+
+    challengeNumber = challenge[pos1: pos2]
+
+    print(f"::set-output name=challengeNumber::{challengeNumber}")
 
 if __name__ == "__main__":
     main()
