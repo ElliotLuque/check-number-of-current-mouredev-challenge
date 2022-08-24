b5f964c2a9776f353e832d4d56e0c747c6881039
ElliotLuque <elliotluque@gmail.com>
2022-08-25 01:08:53 +0200

    Linted python code

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ def main():
 
     soup = BeautifulSoup(req.content, 'html.parser')
 
-    challenge = soup.select('section.palette-1.section.section-left > div > div > article > h4 > span > span')[0].text
+    challenge = soup.select("""section.palette-1.section.section-left > div > 
+                            div > article > h4 > span > span""")[0].text
 
     pos1 = challenge.find("#") + 1
     pos2 = challenge.find(":", pos1)
@@ -17,6 +18,7 @@ def main():
     challengeNumber = challenge[pos1: pos2]
 
     print(f"::set-output name=challengeNumber::{challengeNumber}")
+
 
 if __name__ == "__main__":
     main()
